502bb6da68e2f70c6286e4fcb6b939685a568e0e
Mercy <mercycheruiyot2013@gmail.com>
2022-10-26 21:17:33 +0300

    Completed assignement

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user"
+class Student < User
+    
+attr_accessor :knowledge
+    def initialize
+        @knowledge = []
+      end
+      def learn(knowledge)
+        @knowledge << knowledge
 
+      end
+    
 end
+# steve = Student.new
+# steve.first_name = "Steve"
+# steve.last_name = "Jobs"
